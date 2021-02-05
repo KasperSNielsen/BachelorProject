d8d215af82213689efbdb00dc0cc84b0e75aa7d2
Kasper S. Nielsen (laptop) <Kasper@s-nielsen.dk>
2021-02-05 12:14:02 +0100

    hacspec learning

--- rust_testing/hacspec_testing/src/first.rs
+++ rust_testing/hacspec_testing/src/first.rs
@@ -1,8 +1,33 @@
 use hacspec_lib::*;
-bytes!(tau, 8);
 
-pub fn three() -> i32 {
-    let x = 3;
+const IVSIZE: usize = 12;
+bytes!(Nonce, IVSIZE);
+
+bytes!(Tau, 8);
+
+public_nat_mod!(
+    type_name: Scalar,
+    type_of_canvas: ScalarCanvas,
+    bit_size_of_field: 256,
+    modulo_value: "8000000000000000000000000000000000000000000000000000000000000000"
+);
+
+
+pub fn three(nonce: Nonce) -> BigInt {
+    
+    let test = Tau::new();
+    let mut nonce2 = nonce;
+    nonce2[1] = U8::classify(67);
+
+    let x = BigInt::from_literal(12u128);   
+    let one = BigInt::ONE();
+    let x = x.wrap_add(one); //x = x+1, ie 13. Note: comsumes x so need to assign to new one
+    // 
+
+    //let one: BigInt = BigInt::ZERO();
+
+
     x
+
 }
 
--- rust_testing/hacspec_testing/tests/test_first.rs
+++ rust_testing/hacspec_testing/tests/test_first.rs
@@ -1,6 +1,23 @@
 use hacspec_testing::*;
+use hacspec_lib::*;
+
 #[test]
 fn test_three() 
 {
-    assert_eq!(3, three())
+
+    let x = U32::classify(1u32);
+
+    let nonce2 = Nonce::from_public_slice(&[
+        0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb,
+    ]); //Create array of type Nonce
+    let x: BigInt = three(nonce2);
+    
+    /*let y: U8 = nonce[1];
+    assert_eq!(y.declassify(), 67);
+    assert_eq!(declassify_u8_from_U8(nonce[1]), 67);
+    for i in 0..12 {
+        println!("{}", nonce[i]) //Printing the stuff from first.rs
+    }
+    */
+    println!("{}", x) //Printing the stuff from first.rs
 }
