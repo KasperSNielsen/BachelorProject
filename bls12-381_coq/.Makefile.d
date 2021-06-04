183a1fec5c5c8d91a8d31726f6470a1af99ccd92
Kasper S. Nielsen (laptop) <Kasper@s-nielsen.dk>
2021-06-04 14:55:27 +0200

    fix makefile

--- bls12-381_coq/.Makefile.d
+++ bls12-381_coq/.Makefile.d
@@ -1,12 +1,12 @@
 blsprime.vo blsprime.glob blsprime.v.beautified blsprime.required_vo: blsprime.v 
 blsprime.vio: blsprime.v 
 blsprime.vos blsprime.vok blsprime.required_vos: blsprime.v 
-bls.vo bls.glob bls.v.beautified bls.required_vo: bls.v Lib.vo IntTypes.vo blsprime.vo
-bls.vio: bls.v Lib.vio IntTypes.vio blsprime.vio
-bls.vos bls.vok bls.required_vos: bls.v Lib.vos IntTypes.vos blsprime.vos
-IntTypes.vo IntTypes.glob IntTypes.v.beautified IntTypes.required_vo: IntTypes.v 
-IntTypes.vio: IntTypes.v 
-IntTypes.vos IntTypes.vok IntTypes.required_vos: IntTypes.v 
-Lib.vo Lib.glob Lib.v.beautified Lib.required_vo: Lib.v IntTypes.vo
-Lib.vio: Lib.v IntTypes.vio
-Lib.vos Lib.vok Lib.required_vos: Lib.v IntTypes.vos
+bls.vo bls.glob bls.v.beautified bls.required_vo: bls.v Lib.vo MachineIntegers.vo blsprime.vo
+bls.vio: bls.v Lib.vio MachineIntegers.vio blsprime.vio
+bls.vos bls.vok bls.required_vos: bls.v Lib.vos MachineIntegers.vos blsprime.vos
+Lib.vo Lib.glob Lib.v.beautified Lib.required_vo: Lib.v MachineIntegers.vo
+Lib.vio: Lib.v MachineIntegers.vio
+Lib.vos Lib.vok Lib.required_vos: Lib.v MachineIntegers.vos
+MachineIntegers.vo MachineIntegers.glob MachineIntegers.v.beautified MachineIntegers.required_vo: MachineIntegers.v 
+MachineIntegers.vio: MachineIntegers.v 
+MachineIntegers.vos MachineIntegers.vok MachineIntegers.required_vos: MachineIntegers.v 
